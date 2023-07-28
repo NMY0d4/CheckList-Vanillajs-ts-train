56e6f33ec7558169e3f69341f6ddeebd1b199add
Grégory Marini <greg.marini@hotmail.fr>
2023-07-28 11:22:24 +0200

    complete ListTemplate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import './css/style.css';
+import FullList from './model/FullList';
 import ListItem from './model/ListItem';
-
-const item = new ListItem('id1', 'apprendre le code', false);
-console.log(item.item);
-item.checked = true;
-console.log(item.checked);
+import ListTemplate from './templates/ListTemplate';
--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -9,6 +9,8 @@ interface DOMList {
 export default class ListTemplate implements DOMList {
   ul: HTMLUListElement;
 
+  static instance: ListTemplate = new ListTemplate();
+
   private constructor() {
     this.ul = document.getElementById('listItems') as HTMLUListElement;
   }
@@ -49,6 +51,7 @@ export default class ListTemplate implements DOMList {
         fullList.removeItem(item.id);
         this.render(fullList);
       });
+      this.ul.append(li);
     });
   }
 }
